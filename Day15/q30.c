9a8b46f478bccd361e3393f66c37bd0f26b3a50c
Nikhil-Nara <nikhil.34686@stu.upes.ac.in>
2026-08-24 01:00:00 +0530

    Day 15

---
+++ Day15/q30.c
@@ -0,0 +1,34 @@
+//Q30: Write a program to reverse a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4321
+
+Input 2:
+100
+Output 2:
+1
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int n, rev = 0, r;
+
+    scanf("%d", &n);
+
+    while(n != 0)
+    {
+        r = n % 10;
+        rev = rev * 10 + r;
+        n = n / 10;
+    }
+
+    printf("%d", rev);
+
+    return 0;
+}
