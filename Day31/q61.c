741bf8d68818948b47121027b37d85f082d69283
Nikhil-Nara <nikhil.34686@stu.upes.ac.in>
2026-09-09 07:51:46 +0530

    Day 31

---
+++ Day31/q61.c
@@ -0,0 +1,48 @@
+//Q61: Search for an element in an array using linear search.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+3
+Output 1:
+Found at index 2
+
+Input 2:
+4
+10 20 30 40
+25
+Output 2:
+-1
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, x, index = -1;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &x);
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == x)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if(index == -1)
+        printf("-1");
+    else
+        printf("Found at index %d", index);
+
+    return 0;
+}
