584efa0eb21ce568f77d041248d91de0bf01ba8e
Nikhil-Nara <nikhil.34686@stu.upes.ac.in>
2026-09-13 01:00:00 +0530

    Day 35

---
+++ Day35/q69.c
@@ -0,0 +1,39 @@
+//Q69: Find the second largest element in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+10 20 30 40 50
+Output 1:
+40
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, j, temp;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] < a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    printf("%d", a[1]);
+
+    return 0;
+}
